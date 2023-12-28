765723bee152302908dbe66c7592fc3d59933d4c
imsaifun <imsaifun@gmail.com>
2023-12-29 00:15:56 +0400

    working

--- assets/theme.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded",()=>{  
-  function onChangeSubmitHandler(event){
-    event.target.form.submit();
-  }
-  document.querySelectorAll('.shopify-currency-form, .language_selector').forEach((elm)=>{
-    elm.addEventListener('change',onChangeSubmitHandler);
-  })
-})
